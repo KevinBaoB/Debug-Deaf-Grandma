400746c5a66f4a38ef78e65a66584744d998612c
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 16:50:36 -0500

    First

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-let continue = true
+function deafGrandma() {
+  let go = true;
+  let goodbyes = 0;
+  while (go) {
+    let userInput = prompt();
 
-while ( continue ) {
-
-    let goodbyes = 0
-    let userInput = prompt()
-
-    if ( userInput = "" ) {
-        alert("WHAT!?")
+    if (userInput === "") {
+      alert("WHAT!?");
+    } else if (userInput.toUpperCase() != userInput) {
+      alert("SPEAK UP, KID!");
+    } else if (
+      userInput.toUpperCase() === userInput &&
+      userInput != "GOODBYE!"
+    ) {
+      alert("NO, NOT SINCE 1946!");
+    } else if (userInput == "GOODBYE!") {
+      goodbyes += 1;
+      if (goodbyes == 1) {
+        alert("LEAVING SO SOON?");
+        console.log(goodbyes);
+      } else if (goodbyes == 2) {
+        alert("LATER, SKATER!");
+        console.log(goodbyes);
+        go = false;
+      }
     }
-    else if ( userinput.toUppercase != statement ) {
-        alert("SPEAK UP, KID!")
-    }
-    else if ( statement.toUppercase == statement ) {
-        alert('NO, NOT SINCE 1946!')
-    }
-    else if ( userinput == "GOODBYE!" ) {
-        goodbyes + 1
-
-        if ( goodbyes == 1 ) {
-            alert("LEAVING SO SOON?')
-        }
-        else if ( goodbyes == 2 ) {
-            alert("LATER, SKATER!")
-            continue = false
-        }
-    }
+  }
 }
+deafGrandma();
